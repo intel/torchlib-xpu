ff9ee1f4a7c988ffcfa3e4e6720a26042485bb83
Felipe Leza Alvarez <felipe.leza.alvarez@intel.com>
2026-06-15 11:29:05 -0600

    Update packages/fbgemm-xpu/src/fbgemm_xpu/ops_registry.cpp
    Co-authored-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- packages/fbgemm-xpu/src/fbgemm_xpu/ops_registry.cpp
+++ packages/fbgemm-xpu/src/fbgemm_xpu/ops_registry.cpp
@@ -1,14 +1,6 @@
 /*
- * Copyright 2026 Intel Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Portions of this file are derived from FBGEMM
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Copyright (c) Meta Platforms, Inc. and affiliates. All rights reserved.
+ * Copyright (c) 2026 Intel Corporation. All Rights Reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  */
  
